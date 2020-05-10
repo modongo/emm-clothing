4a36750a940690931efa885b739a9968c96af533
Michael Odongo <mikodongo@gmail.com>
2020-05-10 05:40:14 +0300

    added routing to the hats page using HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import HomePage from './homepage.component';
+import {Switch, Route} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => (
+
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
-    <HomePage/>
+   <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route  path='/hats' component={HatsPage} />
+   </Switch>
     );
 }
 export default App;
